861925ddbebb00dd4a37d80681a4a3e8b4f9556e
sunkai <1403417950@qq.com>
2019-10-18 17:51:29 +0800

    第一个java文件

---
+++ src/com/sun/leet/array/Test.java
@@ -0,0 +1,15 @@
+package com.sun.leet.array;
+
+/**
+ * @Author: 孙凯
+ * @Date: 2019/10/18 17:50
+ * @Description:
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        int a = 22;
+        int b = 32454;
+        System.out.println(a + b);
+    }
+}
